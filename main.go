8b52cd5f370678420cbc05e3a6b6adbbf38cbefb
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-09-04 11:32:23 -0700

    refactor: Update server startup message with URL

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Starting server on :8080...")
+	fmt.Println("Starting server on http://localhost:8080...")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
