0815eadc6a3505569628b0b363a056de29166b87
Chris Vouga <crvouga@gmail.com>
2025-06-05 11:14:48 -0700

    fix: Enhance cache control headers to improve caching behavior and performance

--- app/app.go
+++ app/app.go
@@ -66,9 +66,8 @@ func cacheControl(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Expires", "0")
 		return
 	}
-	w.Header().Set("Cache-Control", "public, max-age=31536000")
+	w.Header().Set("Cache-Control", "public, max-age=31536000, stale-while-revalidate=86400, stale-if-error=86400, immutable")
 	w.Header().Set("Expires", time.Now().AddDate(1, 0, 0).Format(time.RFC1123))
-	w.Header().Set("Vary", "Accept-Encoding")
 }
 
 // newMuxLoggedIn is the mux for the logged in user.
