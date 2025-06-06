fe330063a872f375db5d08f7fe8eb83b40b050be
Chris Vouga <crvouga@gmail.com>
2025-06-05 18:39:32 -0700

    refactor: Enable recommended SQLite pragmas for improved performance and safety during database initialization

--- lib/sqlite/sqlite.go
+++ lib/sqlite/sqlite.go
@@ -12,10 +12,22 @@ func New(dbPath string) (*sql.DB, error) {
 		return nil, err
 	}
 
-	// Enable foreign key constraints
-	_, err = db.Exec("PRAGMA foreign_keys = ON")
-	if err != nil {
-		return nil, err
+	// Enable recommended pragmas for better performance and safety
+	pragmas := []string{
+		"PRAGMA foreign_keys = ON",
+		"PRAGMA journal_mode = WAL",
+		"PRAGMA synchronous = NORMAL",
+		"PRAGMA temp_store = MEMORY",
+		"PRAGMA mmap_size = 30000000000",
+		"PRAGMA cache_size = -2000",
+		"PRAGMA busy_timeout = 5000",
+	}
+
+	for _, pragma := range pragmas {
+		_, err = db.Exec(pragma)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return db, nil
