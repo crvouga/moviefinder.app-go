f5d30102613eb17a21a64b78f6f7036fe830f060
Chris Vouga <crvouga@gmail.com>
2025-06-05 19:02:17 -0700

    refactor: Improve swiper component layout and enhance slide handling for better visual consistency

--- app/media/mediaDB/loader.go
+++ app/media/mediaDB/loader.go
@@ -2,6 +2,7 @@ package mediaDB
 
 import (
 	"database/sql"
+	"io"
 	"log"
 	"log/slog"
 	"movieFinder/lib/tmdbAPI"
@@ -9,7 +10,8 @@ import (
 
 func Loader(db *sql.DB, client *tmdbAPI.Client, maxPages int, done chan struct{}, logger *slog.Logger) error {
 	logger.Info("starting media loader", "maxPages", maxPages)
-	err := LoaderTmdbAPIDiscoverMovie(db, client, maxPages, done, logger)
+	noopLogger := slog.New(slog.NewTextHandler(io.Discard, nil))
+	err := LoaderTmdbAPIDiscoverMovie(db, client, maxPages, done, noopLogger)
 	if err != nil {
 		log.Printf("Error in media loader: %v", err)
 		return err
