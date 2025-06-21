73faf2641b55d65710476a2b1b1eeb7ecb7f18bf
Chris Vouga <crvouga@gmail.com>
2025-06-21 14:16:01 -0700

    refactor: Improve upsertEntity SQL syntax for clarity and consistency in handling conflicts

--- app/entityDB/upsertEntity.sql
+++ app/entityDB/upsertEntity.sql
@@ -3,7 +3,12 @@ INSERT INTO entities (
     type,
     data,
     updated_at_epoch
-) VALUES ($1, $2, $3, EXTRACT(EPOCH FROM NOW()))
-ON CONFLICT(id, type) DO UPDATE SET
+) VALUES (
+    $1,
+    $2,
+    $3,
+    EXTRACT(EPOCH FROM NOW())
+)
+ON CONFLICT (id, type) DO UPDATE SET
     data = EXCLUDED.data,
     updated_at_epoch = EXCLUDED.updated_at_epoch
