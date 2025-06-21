143e0479ab76bfb54edc6f16f3c6372d6df4a7bf
Chris Vouga <crvouga@gmail.com>
2025-06-20 18:56:39 -0700

    refactor: Add PreloadImageSrc to FeedSwiperSlide for improved image loading in feed swiper

--- app/feed/feedPage/feedSwiperSlides/feedSwiperSlides.go
+++ app/feed/feedPage/feedSwiperSlides/feedSwiperSlides.go
@@ -8,9 +8,10 @@ import (
 )
 
 type FeedSwiperSlide struct {
-	ImageSrc  string
-	URL       string
-	FeedIndex int64
+	ImageSrc        string
+	PreloadImageSrc string
+	URL             string
+	FeedIndex       int64
 }
 
 var TemplateName = "feedSwiperSlides"
@@ -18,8 +19,9 @@ var TemplatePath = static.GetSiblingPath(TemplateName + ".html")
 
 func FromMedia(m media.Media, feedIndex int64) FeedSwiperSlide {
 	return FeedSwiperSlide{
-		ImageSrc:  m.PosterURL,
-		URL:       routes.MediaPage(m.ID),
-		FeedIndex: feedIndex,
+		ImageSrc:        m.PosterURL,
+		PreloadImageSrc: m.BackdropURL,
+		URL:             routes.MediaPage(m.ID),
+		FeedIndex:       feedIndex,
 	}
 }
