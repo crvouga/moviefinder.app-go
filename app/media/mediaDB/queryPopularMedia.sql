0e5a9f19f59a6e053ee3d1c68afc176dd8ad2c79
Chris Vouga <crvouga@gmail.com>
2025-09-27 15:59:34 -0700

    fix: Update queryPopularMedia.sql to filter out media with null or empty poster URLs

--- app/media/mediaDB/queryPopularMedia.sql
+++ app/media/mediaDB/queryPopularMedia.sql
@@ -1,12 +1,15 @@
 SELECT
-	m.id,
-	m.title,
-	m.description,
-	m.popularity,
+    m.id,
+    m.title,
+    m.description,
+    m.popularity,
     m.poster_url,
     m.backdrop_url
 FROM media_denormalized_v m
-WHERE m.is_adult = false
+WHERE 
+		m.is_adult = false
+    AND	m.poster_url IS NOT NULL
+    AND	m.poster_url != ''
 ORDER BY m.popularity DESC
 LIMIT $1
 OFFSET $2
